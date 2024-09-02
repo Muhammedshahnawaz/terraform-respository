e55d8d75a5f88036da31b3407a5138b49e5357fa
Muhammedshahnawaz <158474991+Muhammedshahnawaz@users.noreply.github.com>
2024-09-02 18:21:50 +0530

    Add files via upload

---
+++ variable.tf
@@ -0,0 +1,19 @@
+variable "filename" {
+    default = "pets.txt"
+    type = string
+  
+}
+variable "content" {
+    default = "i love birds"
+  type = string
+}
+
+variable "prefix" {
+    default = "Mr"
+    type = string
+}
+variable "length" {
+    default = "1"
+    type = number
+  
+}
